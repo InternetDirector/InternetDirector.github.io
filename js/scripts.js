0a2ffca932d679a8ba0b8fd81767d63ca9c7885c
InternetDirector <65730640+InternetDirector@users.noreply.github.com>
2021-06-14 20:43:58 +0300

    дора ботки

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 function Function113(){
-var A = prompt('Введите A', 100);
-var B = prompt('Введите B', 100);
-var C = prompt('Введите C', 100);
+var A = document.ELTH.A.value;
+var B = document.ELTH.B.value;
+var C = document.ELTH.C.value;
 if(A<B&&B<C)
 {
 A*=A;
@@ -15,4 +15,43 @@ B*=-1;
 C*=-1;
 }
 alert('A= '+ A+' B= '+ B+' C= '+C)
-}
+};
+function Function114(){
+	 var A=document.ELFO.vesh_n.value;
+	 console.log(A);
+	 var N=document.ELFO.cel_n.value>0?document.ELFO.cel_n.value:true;
+	 if(N==true)
+		 return;
+ while(A < N){
+	 A*=A;
+	 if(A % 1==0)
+			 alert(A);
+ }
+};
+function modification(action){
+switch(action) {
+  case 1:
+    document.all["p1"].style.fontSize="150%";
+    break;
+  case 0:
+    document.all["p1"].style.fontSize="100%";
+    break;
+	case 2:
+    document.all["p1"].style.fontSize="64%";
+    break;
+  default:
+    break;
+}};
+function cngBg(clr){
+document.bgColor=clr;
+};
+function ChangeColor(Element) {
+	if (Element.style.backgroundColor == 'white') Element.style.backgroundColor = 'green';
+	else Element.style.backgroundColor = 'white';
+	return false;
+};
+function Changetxtcol(Element) {
+	if (Element.style.color == '') Element.style.color = 'green';
+	else Element.style.color = '';
+	return false;
+};
